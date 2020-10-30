c3dce6f7a5cf169c9d9a3054036a54553807abc9
shahin1290 <s.patowary@yahoo.com>
2020-10-30 15:33:53 +0100

    Adds error handling for catching asycn functions

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,70 +1,51 @@
 const Article = require('../models/Article')
+const catchAsync = require('./../utils/catchAsync')
 
-module.exports.index = async (req, res) => {
-  try {
-    const articles = await Article.find()
+module.exports.index = catchAsync(async (req, res) => {
+  const articles = await Article.find()
 
-    res.status(200).json({
-      status: 'success',
-      results: articles.length,
-      data: { articles },
-    })
-  } catch (err) {
-    res.status(400).json({ status: 'fail', message: err })
-  }
-}
+  res.status(200).json({
+    status: 'success',
+    results: articles.length,
+    data: { articles },
+  })
+})
 
-module.exports.show =  async (req, res) => {
-  try {
-    const article = await Article.findById(req.params.id)
+module.exports.show = catchAsync(async (req, res) => {
+  const article = await Article.findById(req.params.id)
 
-    res.status(200).json({
-      status: 'success',
-      data: { article },
-    })
-  } catch (err) {
-    res.status(404).json({ status: 'fail', message: err })
-  }
-}
+  res.status(200).json({
+    status: 'success',
+    data: { article },
+  })
+})
 
-module.exports.create = async (req, res) => {
-  try {
-    const newArticle = await Article.create(req.body)
+module.exports.create = catchAsync(async (req, res) => {
+  const newArticle = await Article.create(req.body)
 
-    res.status(201).json({
-      status: 'success',
-      data: { article: newArticle },
-    })
-  } catch (err) {
-    res.status(400).json({ status: 'fail', message: err })
-  }
-}
+  res.status(201).json({
+    status: 'success',
+    data: { article: newArticle },
+  })
+})
 
-module.exports.update = async (req, res) => {
-  try {
-    const article = await Article.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    })
+module.exports.update = catchAsync(async (req, res) => {
+  const article = await Article.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
 
-    res.status(200).json({
-      status: 'success',
-      data: { article },
-    })
-  } catch (err) {
-    res.status(404).json({ status: 'fail', message: err })
-  }
-}
+  res.status(200).json({
+    status: 'success',
+    data: { article },
+  })
+})
 
-module.exports.destroy = async (req, res) => {
-  try {
-    await Article.findByIdAndDelete(req.params.id)
+module.exports.destroy = catchAsync(async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id)
 
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    })
-  } catch (err) {
-    res.status(404).json({ status: 'fail', message: err })
-  }
-}
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  })
+})
