9d9adae188a07c3625640d9fb6d509851cea7fdd
shahin1290 <s.patowary@yahoo.com>
2020-10-30 15:20:00 +0100

    Adds appError class and global error handling middleware

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const articleRoutes = require('./routes/articles')
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 
 const app = express()
 app.use(express.json())
@@ -7,12 +9,9 @@ app.use(express.json())
 app.use('/api/v1/articles', articleRoutes)
 
 app.all('*', (req, res, next) => {
-  res
-    .status(404)
-    .json({
-      status: 'fail',
-      message: `Can't find ${req.originalUrl} on this server!`,
-    })
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`), 404)
 })
+
+app.use(globalErrorHandler)
 
 module.exports = app
